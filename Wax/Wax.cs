2dd2deec5f0ca8d9a8733f8f4c39351fb30a1cb8
yewnyx <yewnyx@yewnyx.xyz>
2021-11-13 04:47:06 -0800

    WIP instance.c conversion

--- Wax/Wax.cs
+++ Wax/Wax.cs
@@ -14,6 +14,12 @@ namespace Wax {
                 public ulong size;
                 public IntPtr data;
             }
+
+            [StructLayout(LayoutKind.Sequential, Size = 16)]
+            public struct extern_vec_t {
+                public ulong size;
+                public IntPtr data;
+            }
         }
 
         internal unsafe partial struct __wasm {
@@ -23,16 +29,35 @@ namespace Wax {
             [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_store_new", CallingConvention = CallingConvention.Cdecl)]
             internal static extern IntPtr store_new(IntPtr engine);
 
+            [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_module_new", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern IntPtr module_new(IntPtr store, ref byte_vec_t binary);
+
+            [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_instance_new", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern IntPtr instance_new(IntPtr store, IntPtr module, ref extern_vec_t imports, IntPtr _);
+
+            [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_byte_vec_new", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void byte_vec_new(ref byte_vec_t vec, ulong length, ref byte buffer);
+
             [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_byte_vec_new_uninitialized", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern void byte_vec_new_uninitialized(ref byte_vec_t vec, int capacity);
+            internal static extern void byte_vec_new_uninitialized(ref byte_vec_t vec, ulong capacity);
+
+            [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_byte_vec_delete", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void byte_vec_delete(ref byte_vec_t vec);
+
+            [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasm_byte_vec_copy", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void byte_vec_copy(ref byte_vec_t vec, ref byte_vec_t vec2);
         }
 
         internal unsafe partial struct __wasmer {
             [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasmer_last_error_length", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern int last_error_length();
+            internal static extern ulong last_error_length();
 
             [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wasmer_last_error_message", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern int last_error_message(ref char buffer, int length);
+            internal static extern ulong last_error_message(ref byte buffer, ulong length);
+
+            [SuppressUnmanagedCodeSecurity, DllImport("wasmer", EntryPoint = "wat2wasm", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void wat2wasm(ref byte_vec_t wat, ref byte_vec_t @out);
+
         }
 
         internal unsafe partial struct __wasi { }
--- WaxExample/WaxExample.cs
+++ WaxExample/WaxExample.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 using Wax.Imports;
 using Wax.Imports.Data;
 
@@ -7,16 +8,54 @@ namespace Wax.WaxExample {
     class WaxExample {
         static void print_wasmer_error() {
             var error_len = __wasmer.last_error_length();
-            Span<char> error_str = stackalloc char[error_len];
+            Span<byte> error_str = stackalloc byte[(int)error_len];
             __wasmer.last_error_message(ref MemoryMarshal.GetReference(error_str), error_len);
+            Console.Error.WriteLine(error_str.ToString());
         }
 
         static void Main(string[] args) {
+            var wat_string = @"
+(module
+  (type $add_one_t (func (param i32) (result i32)))
+  (func $add_one_f (type $add_one_t) (param $value i32) (result i32)
+    local.get $value
+    i32.const 1
+    i32.add)
+  (export ""add_one"" (func $add_one_f)))
+";
+            var wat_string_utf8 = Encoding.UTF8.GetBytes(wat_string);
+            byte_vec_t wat = default;
+            __wasm.byte_vec_new(ref wat, (ulong)wat_string_utf8.Length, ref MemoryMarshal.GetReference(wat_string_utf8.AsSpan()));
+            byte_vec_t wasm_bytes = default;
+            __wasmer.wat2wasm(ref wat, ref wasm_bytes);
+
+
             var engine = __wasm.engine_new();
             var store = __wasm.store_new(engine);
+            var module = __wasm.module_new(store, ref wasm_bytes);
+            if (module == null) {
+                print_wasmer_error();
+                Environment.Exit(1);
+            }
 
-            byte_vec_t bv = default;
-            __wasm.byte_vec_new_uninitialized(ref bv, 10);
+            extern_vec_t imports = new extern_vec_t { size = 0, data = IntPtr.Zero };
+            var instance = __wasm.instance_new(store, module, ref imports, IntPtr.Zero);
+
+            // Pick work back up here
+
+
+            // Delete this in next commit, just leaving it here for one commit for posterity, to be moved into a unit test or something later
+            {
+                byte_vec_t bv = default;
+                __wasm.byte_vec_new_uninitialized(ref bv, 10);
+                byte_vec_t bv2 = default;
+                byte_vec_t bv3 = default;
+                __wasm.byte_vec_copy(ref bv2, ref bv);
+                __wasm.byte_vec_delete(ref bv);
+                __wasm.byte_vec_copy(ref bv3, ref bv2);
+                __wasm.byte_vec_delete(ref bv3);
+                __wasm.byte_vec_delete(ref bv2);
+            }
         }
     }
 }
